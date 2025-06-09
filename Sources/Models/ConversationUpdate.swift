40ab95ee35a685010eb86162924e2055096ddccf
Joris Abalea <joris.abalea@gmail.com>
2025-06-09 13:27:03 +0900

    Add missing fields in ConversationUpdate

--- Sources/Models/ConversationUpdate.swift
+++ Sources/Models/ConversationUpdate.swift
@@ -5,10 +5,32 @@ public struct Message: Codable {
         case user = "user"
         case assistant = "assistant"
         case system = "system"
+        case tool = "tool"
+        case toolCalls = "tool_calls"
     }
     
+    public struct ToolCall: Codable {
+        public struct Function: Codable {
+            public let name: String
+            public let arguments: String
+        }
+
+        public let id: String
+        public let type: String
+        public let function: Function
+    }
+
     public let role: Role
-    public let content: String
+    public let content: String?
+    public let toolCalls: [ToolCall]?
+    public let toolCallId: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case role
+        case content
+        case toolCalls = "tool_calls"
+        case toolCallId = "tool_call_id"
+    }
 }
 
 public struct ConversationUpdate: Codable {
