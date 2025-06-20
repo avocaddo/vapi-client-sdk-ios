c01914ef35e7473124f0342230e6d93516b0383d
avocaddo <joris.abalea+github@gmail.com>
2025-06-20 11:20:48 +0900

    Add the monitoring

--- Sources/Models/WebCallResponse.swift
+++ Sources/Models/WebCallResponse.swift
@@ -11,8 +11,14 @@ public struct ArtifactPlan: Decodable {
     public let videoRecordingEnabled: Bool
 }
 
+public struct Monitor: Decodable {
+    public let listenUrl: String
+    public let controlUrl: String
+}
+
 public struct WebCallResponse: Decodable {
     let webCallUrl: URL
     public let id: String
     public let artifactPlan: ArtifactPlan?
+    public let monitor: Monitor?
 }
