5e7f1089d11a67ee58aef5b805cdf25668830481
avocaddo <joris.abalea+github@gmail.com>
2026-01-21 13:56:25 +0900

    Add the missing assistant in status

---
+++ Sources/Models/AnyCodable.swift
@@ -0,0 +1,33 @@
+// Helper to decode Any values
+public struct AnyCodable: Codable {
+    public let value: Any
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let string = try? container.decode(String.self) {
+            value = string
+        } else if let int = try? container.decode(Int.self) {
+            value = int
+        } else if let double = try? container.decode(Double.self) {
+            value = double
+        } else if let bool = try? container.decode(Bool.self) {
+            value = bool
+        } else {
+            value = [:]
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        if let string = value as? String {
+            try container.encode(string)
+        } else if let int = value as? Int {
+            try container.encode(int)
+        } else if let double = value as? Double {
+            try container.encode(double)
+        } else if let bool = value as? Bool {
+            try container.encode(bool)
+        }
+    }
+}
--- Sources/Models/StatusUpdate.swift
+++ Sources/Models/StatusUpdate.swift
@@ -1,5 +1,26 @@
 import Foundation
 
 public struct StatusUpdate: Codable {
+    public struct Assistant: Codable {
+        // Add all the fields that might appear, but make them optional
+        public let variableValues: [String: AnyCodable]?
+        
+        // Use CodingKeys to ignore unknown fields
+        private enum CodingKeys: String, CodingKey {
+            case variableValues
+        }
+        
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            variableValues = try container.decodeIfPresent([String: AnyCodable].self, forKey: .variableValues)
+        }
+    }
+    
     public let status: String
+    public let newAssistant: Assistant?
+    
+    private enum CodingKeys: String, CodingKey {
+        case status
+        case newAssistant
+    }
 }
