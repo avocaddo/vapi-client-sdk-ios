bf3406921743511a98eac200a21e3ea430dd5dbf
avocaddo <joris.abalea+github@gmail.com>
2026-01-21 13:59:29 +0900

    Add more to status

--- Sources/Models/StatusUpdate.swift
+++ Sources/Models/StatusUpdate.swift
@@ -2,4 +2,16 @@ import Foundation
 
 public struct StatusUpdate: Codable {
     public let status: String
+    
+    // Custom decoder to ignore all extra fields like newAssistant
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        // Only decode status, ignore everything else
+        status = try container.decodeIfPresent(String.self, forKey: .status) ?? ""
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case status
+        case type // This is in the JSON but we don't need it
+    }
 }
